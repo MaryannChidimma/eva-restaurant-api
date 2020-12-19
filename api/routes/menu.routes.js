4dc10f4bb0edaf53ef21316f8ea2b77ecd88875a
Okonkwo Maryann Chidimma <you@example.com>
2020-12-19 17:53:17 +0100

    made little corrections

--- api/routes/menu.routes.js
+++ api/routes/menu.routes.js
@@ -1,12 +1,12 @@
 
 const router = require("express").Router();
 const MenuRoute = require("../controllers/menu.controller");
-
-router.post('/', MenuRoute.create);
+const checkAuth = require('../middleware/adminAuth');
+router.post('/', checkAuth, MenuRoute.create);
 router.get('/',  MenuRoute.getAll);
 router.get('/:foodId', MenuRoute.getOne);
-router.put('/:foodId', MenuRoute.update);
-router.delete('/:foodId', MenuRoute.delete);
+router.put('/:foodId', checkAuth, MenuRoute.update);
+router.delete('/:foodId, checkAuth', MenuRoute.delete);
 
 
 module.exports = router
