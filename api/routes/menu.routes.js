1040a427fd4b7471ce9367bf99375986d20285db
Okonkwo Maryann Chidimma <you@example.com>
2020-12-28 04:26:42 +0100

    added multer

--- api/routes/menu.routes.js
+++ api/routes/menu.routes.js
@@ -1,13 +1,15 @@
+require('express-async-errors');
 const router = require("express").Router();
 const MenuRoute = require("../controllers/menu.controller");
 const checkAuth = require('../middleware/adminAuth');
-const asyncMiddleware = require('../middleware/async')
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/'});
 
-router.post('/', checkAuth, asyncMiddleware(MenuRoute.create));
-router.get('/', asyncMiddleware(MenuRoute.getAll));
-router.get('/:foodId', asyncMiddleware(MenuRoute.getOne));
-router.put('/:foodId', checkAuth, asyncMiddleware(MenuRoute.update));
-router.delete('/:foodId, checkAuth', asyncMiddleware(MenuRoute.delete));
+router.post('/', upload.single('mediafile'), checkAuth, MenuRoute.create);
+router.get('/', MenuRoute.getAll);
+router.get('/:foodId', MenuRoute.getOne);
+router.put('/:foodId', checkAuth, MenuRoute.update);
+router.delete('/:foodId, checkAuth', MenuRoute.delete);
 
 
 module.exports = router
